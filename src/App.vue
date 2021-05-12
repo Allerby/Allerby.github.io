fd1a32198d7eccae959fead09a865ff21bf05916
Michael <michael.lee.allerby@gmail.com>
2021-05-12 18:06:02 +1200

    supabase integration

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="max-w-screen-lg mx-auto mt-20">
+    <h1 class="text-3xl sm:text-5xl lg:text-6xl leading-none font-extrabold text-gray-900 tracking-tight mb-12">
+      Projects
+    </h1>
+    <div class="grid grid-flow-col grid-cols-2 gap-8">
+      <Card v-for="project in projects" :key="project.id" :project="project" />
+    </div>
+  </div>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import Card from './components/card.vue'
+import { supabase } from './lib/supabase'
+
+let { data: projects, error } = await supabase
+  .from('projects')
+  .select('*')
+
+export default {
+  components: {
+    Card
+  },
+  setup() {
+    return {
+      projects: projects,
+    }
+  }
+}
+
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
@@ -15,8 +38,5 @@ import HelloWorld from './components/HelloWorld.vue'
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="relative px-4 py-10 bg-white shadow-md sm:rounded-3xl sm:p-12 bg-gradient-to-r from-indigo-400 to-purple-600 text-white">
+    <h3 class="text-3xl tracking-tight font-extrabold mb-8">{{ project.name }}</h3>
+    <p class="">{{ project.description }}</p>
+  </div>
+</template>
+
+<script>
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    props: {
+      project: {
+        type: Object,
+        required: true,
+      },
+    },
+  })
+</script>
+
+<style scoped>
+</style>
